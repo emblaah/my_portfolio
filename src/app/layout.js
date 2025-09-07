760ada3cb7fd61e828d2520e170c08f843c7407b
Embla Andersson <emblaandersson@yahoo.se>
2025-09-07 23:28:57 +0200

    added personal favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,17 @@
 import "./globals.css";
 import Header from "@/components/ui/Header";
 import Footer from "@/components/ui/Footer";
+import { icons } from "lucide-react";
 
 export const metadata = {
   title: "Embla Andersson - Frontend Developer",
   description: "Portfolio website built with next app",
+  icon: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/icon0.svg", type: "image/svg", sizes: "32x32" },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
